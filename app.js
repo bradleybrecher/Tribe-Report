94f1a491995e34f1c747be3c56633fe014a90ea4
Bradley Brecher <bradley.brecher@gmail.com>
2018-11-19 19:17:53 -0500

    for displaying results

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const bodyParser = require('body-parser');
 const validUrl = require('valid-url');
 const app = express();
 
+const alert= require('alert-node');
+
+
+const mongoose = require('mongoose');
+const Article = mongoose.model('Article');
+
 app.use(require('express-session')({
     secret: 'keyboard cat',
     resave: false,
@@ -34,7 +40,49 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('index');
+ 	res.render('index');
+});
+
+app.post('/', (req,res)=>{
+
+	 if (validUrl.isUri(req.body.link)){
+        console.log('Looks like an URI');
+        // perform the post and render
+        theName = req.body.name;
+        theTitle = req.body.title;
+        theLink = req.body.link;
+        theComment = req.body.comment;
+
+
+        const article = new Article({
+
+        	name: theName,
+        	title: theTitle,
+        	link: theLink,
+        	comment: theComment
+
+        });
+        article.save(function(err) {
+        	if(err){
+        		console.log(err);
+        		
+        	} else {
+        		console.log('saved');
+        	}
+        	
+   		});
+      	Article.find(function(err, articles) {
+      		console.log(articles);
+            res.render('index', {
+                articles: articles
+            });
+        });
+
+
+    } else {
+        console.log('Not a URI');  
+		alert("Not a URI: TODO: Integrate with DOM");
+    }
 });
 
 // app.post('/login', passport.authenticate('local', { successRedirect: '/',
--- db.js
+++ db.js
@@ -6,27 +6,28 @@ const mongoose = require('mongoose');
 // * our site requires authentication...
 // * so users have a username and password
 // * they also can have 0 or more articles
-const userSchema = new mongoose.Schema({
-  // username provided by authentication plugin
-  // password hash provided by authentication plugin
-  articles:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }]
-});
+// const userSchema = new mongoose.Schema({
+//   // username provided by authentication plugin
+//   // password hash provided by authentication plugin
+//   articles:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }]
+// });
 
 // an article to be posted to the feed
 // * items in a list can be deleted or will be automatically deleted month after posted(trust your tribe members)
 const articleSchema = new mongoose.Schema({
   name: {type: String, required: true},
   title: {type: String, required: true},
-  article: {type: String,  required: true},//check if it a url
-  comment: {type: String, required: false},
-  createdAt: {type: Date, required: true}// will display at the bottom
-}, {
-  _id: true
+  link: {type: String,  required: true},//check if it a url
+  comment: {type: String, required: false}
+// ,
+//   // createdAt: {type: Date, required: true}// will display at the bottom
+// }, {
+//   _id: true
 });
 
 
 module.exports = mongoose.model('Article', articleSchema);
-module.exports = mongoose.model('User', userSchema);
+// module.exports = mongoose.model('User', userSchema);
 
 
-// mongoose.connect('mongodb://localhost/finalproject');
+mongoose.connect('mongodb://localhost/tribe');
